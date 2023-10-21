addad2e9663f58a23033c14c6967305db3fcc3be
BanquerigoSherard <sbanquerigo@gmail.com>
2023-10-21 10:38:23 +0800

    fix b ug

--- Dashboard/dashboard.php
+++ Dashboard/dashboard.php
@@ -1,18 +1,18 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php
     session_start();
     include "../Connection/dbconn.php";
-    if (!isset($_SESSION['user_id'])) {
-        header("Location: Login/Login.php");
-    }
+    // if (!isset($_SESSION['user_id'])) {
+    //     header("Location: ../index.php");
+    // }
 
     if (isset($_GET['log'])) {
         if ($_GET['log'] == 'true') {
             session_destroy();
-            header("Location: Login/Login.php");
+            header("Location: ../index.php");
         }
     }
     ?>
@@ -25,6 +25,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
+
 <body>
     <h1>Dashboard</h1>
     <a href="../Login/Login.php" class="styled-link"> Log out </a>
@@ -42,7 +43,7 @@
         </thead>
         <tbody id="listUsers">
             <?php
-            
+
 
             $sql = "SELECT * FROM `registertable`";
             $result = $connection->query($sql);
@@ -63,7 +64,7 @@
                     echo '<button type="button" data-user-id="' . $row['user_id'] . '" class="btn btn-danger deleteBtn">Delete</button>';
                     echo "</td>";
                     echo "</tr>";
-                   
+
                 }
             } else {
                 echo "<tr><td colspan='7'>No data available</td></tr>";
@@ -73,7 +74,8 @@
         </tbody>
     </table>
     <!-- Modal -->
-    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -85,16 +87,16 @@
                 <div class="modal-body">
                     <input type="hidden" name="userID" id="userID">
                     <div class "form-group">
-                    <label for="FirstName">First Name</label>
-                    <input type="text" class="form-control" id="FirstName" placeholder="First Name">
-                    <label for="LastName">Last Name</label>
-                    <input type="text" class="form-control" id="LastName" placeholder="Last Name">
-                    <label for="Gender">Gender</label>
-                    <input type="text" class="form-control" id="Gender" placeholder="Gender">
-                    <label for="Nationality">Nationality</label>
-                    <input type="text" class="form-control" id="Nationality" placeholder="Nationality">
-                    <label for="userEmail">Email</label>
-                    <input type="email" class="form-control" id="userEmail" placeholder="Email">   
+                        <label for="FirstName">First Name</label>
+                        <input type="text" class="form-control" id="FirstName" placeholder="First Name">
+                        <label for="LastName">Last Name</label>
+                        <input type="text" class="form-control" id="LastName" placeholder="Last Name">
+                        <label for="Gender">Gender</label>
+                        <input type="text" class="form-control" id="Gender" placeholder="Gender">
+                        <label for="Nationality">Nationality</label>
+                        <input type="text" class="form-control" id="Nationality" placeholder="Nationality">
+                        <label for="userEmail">Email</label>
+                        <input type="email" class="form-control" id="userEmail" placeholder="Email">
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -105,51 +107,51 @@
         </div>
     </div>
     <script>
-        
-$(document).ready(function () {
-    $(document).on('click', '.saveUser', function (e) {
-        e.preventDefault();
-        var userID = $('#userID').val();
-        var firstname = $('#FirstName').val();
-        var lastname = $('#LastName').val();
-        var gender = $('#Gender').val();
-        var nationality = $('#Nationality').val();
-        var email = $('#userEmail').val();
 
-        $.ajax({
-            async: true,
-            type: "GET",
-            url: "updateUser.php?userID=" + userID + "&FirstName=" + firstname
-             + "&LastName=" + lastname +  "&Gender=" + gender + "&Nationality=" + nationality
-             + "&userEmail=" + email,
-            dataType: "json",
-            success: function (response) {
+        $(document).ready(function () {
+            $(document).on('click', '.saveUser', function (e) {
+                e.preventDefault();
+                var userID = $('#userID').val();
+                var firstname = $('#FirstName').val();
+                var lastname = $('#LastName').val();
+                var gender = $('#Gender').val();
+                var nationality = $('#Nationality').val();
+                var email = $('#userEmail').val();
 
-                $('#editModal').modal('hide');
+                $.ajax({
+                    async: true,
+                    type: "GET",
+                    url: "updateUser.php?userID=" + userID + "&FirstName=" + firstname
+                        + "&LastName=" + lastname + "&Gender=" + gender + "&Nationality=" + nationality
+                        + "&userEmail=" + email,
+                    dataType: "json",
+                    success: function (response) {
 
-                Swal.fire(
-                    'Updated!',
-                    'User has been updated.',
-                    'success'
-                );
+                        $('#editModal').modal('hide');
 
-                var counter = 0;
+                        Swal.fire(
+                            'Updated!',
+                            'User has been updated.',
+                            'success'
+                        );
 
-                $('#listUsers').html('');
+                        var counter = 0;
 
-                for (let i = 0; i < response.length; i++) {
-                    counter++;
+                        $('#listUsers').html('');
 
-                    var userID, firstname, lastname, gender, nationality, email;
+                        for (let i = 0; i < response.length; i++) {
+                            counter++;
 
-                    userID = response[i][0];
-                    firstname = response[i][1];
-                    lastname = response[i][2];
-                    gender = response[i][3];
-                    nationality = response[i][4];
-                    email = response[i][5];
-                 
-                    $('#listUsers').append('<tr>\
+                            var userID, firstname, lastname, gender, nationality, email;
+
+                            userID = response[i][0];
+                            firstname = response[i][1];
+                            lastname = response[i][2];
+                            gender = response[i][3];
+                            nationality = response[i][4];
+                            email = response[i][5];
+
+                            $('#listUsers').append('<tr>\
                             <th scope="row">'+ counter + '</th >\
                             <td>'+ userID + '</td>\
                             <td>'+ firstname + '</td>\
@@ -163,96 +165,98 @@ $(document).ready(function () {
                             </td >\
                         </tr>');
 
-                }
+                        }
 
 
-            }
+                    }
 
-        });
-
-
-    });
-$(document).on('click', '.editBtn', function (e) {
-        e.preventDefault();
-        var userID = $(this).val();
-
-        $.ajax({
-            async: true,
-            type: "GET",
-            url: "editUser.php?userID=" + userID + "&FirstName=" + firstname
-             + "&LastName=" + lastname +  "&Gender=" + gender + "&Nationality=" + nationality
-             + "&userEmail=" + email,
-            dataType: "json",
-            contentType: false,
-            processData: false,
-            success: function (response) {
-                var userId, firstname, lastname, gender, nationality, email;
-
-                 userId = response[0];
-                 firstname = response[1]; 
-                 lastname = response[2];
-                 gender = response[3];
-                 nationality = response[4];
-                 email = response[5];
-
-                $('#userID').val(userID);
-                $('#FirstName').val(firstname);
-                $('#LastName').val(lastname);
-                $('#Gender').val(gender);
-                $('#Nationality').val(nationality);
-                $('#userEmail').val(email);
-                
-                $('#editModal').modal('show');
+                });
 
 
-            }
+            });
 
-        });
-
-
-    });
-
-    $(document).on('click', '.deleteBtn', function (e) {
-    e.preventDefault();
-    var userID = $(this).data('user-id');
-
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
+            $(document).on('click', '.editBtn', function (e) {
+                e.preventDefault();
+                var userID = $(this).val();
 
                 $.ajax({
                     async: true,
                     type: "GET",
-                    url: "deleteUser.php?userID=" + userID,
+                    url: "editUser.php?userID=" + userID + "&FirstName=" + firstname
+                        + "&LastName=" + lastname + "&Gender=" + gender + "&Nationality=" + nationality
+                        + "&userEmail=" + email,
                     dataType: "json",
+                    contentType: false,
+                    processData: false,
                     success: function (response) {
+                        var userId, firstname, lastname, gender, nationality, email;
 
-                        Swal.fire(
-                            'Deleted!',
-                            'User has been deleted.',
-                            'success'
-                        );
-                        var counter = 0;
-                        $('#listUsers').html('');
-                        for (let i = 0; i < response.length; i++) {
-                        counter++;
-                         var id, firstname, lastname, gender, nationality, email;
+                        userId = response[0];
+                        firstname = response[1];
+                        lastname = response[2];
+                        gender = response[3];
+                        nationality = response[4];
+                        email = response[5];
 
-                        id = response[i][0];
-                        firstname = response[i][1];
-                        lastname = response[i][2];
-                        gender = response[i][3];
-                        nationality = response[i][4];
-                        email = response[i][5];
-                    
-                        $('#listUsers').append('<tr>\
+                        $('#userID').val(userID);
+                        $('#FirstName').val(firstname);
+                        $('#LastName').val(lastname);
+                        $('#Gender').val(gender);
+                        $('#Nationality').val(nationality);
+                        $('#userEmail').val(email);
+
+                        $('#editModal').modal('show');
+
+
+                    }
+
+                });
+
+
+            });
+
+            $(document).on('click', '.deleteBtn', function (e) {
+                e.preventDefault();
+                var userID = $(this).data('user-id');
+
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+
+                        $.ajax({
+                            async: true,
+                            type: "GET",
+                            url: "deleteUser.php?userID=" + userID,
+                            dataType: "json",
+                            success: function (response) {
+
+                                Swal.fire(
+                                    'Deleted!',
+                                    'User has been deleted.',
+                                    'success'
+                                );
+
+                                var counter = 0;
+                                $('#listUsers').html('');
+                                for (let i = 0; i < response.length; i++) {
+                                    counter++;
+                                    var id, firstname, lastname, gender, nationality, email;
+
+                                    id = response[i][0];
+                                    firstname = response[i][1];
+                                    lastname = response[i][2];
+                                    gender = response[i][3];
+                                    nationality = response[i][4];
+                                    email = response[i][5];
+
+                                    $('#listUsers').append('<tr>\
                                 <th scope="row">'+ counter + '</th >\
                                 <td>'+ firstname + '</td>\
                                 <td>'+ lastname + '</td>\
@@ -266,18 +270,19 @@ $(document).on('click', '.editBtn', function (e) {
                             </tr>');
 
 
-                        }
+                                }
+
+                            }
+
+                        });
 
                     }
+                })
 
-                });
-
-            }
-        })
-
-    });
-});
-</script>
+            });
+        });
+    </script>
 
 </body>
+
 </html>
